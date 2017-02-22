57c2bce731b9f374e68b1c50f9898b40ff87150d
jm652564 <jasonqiaomeng@outlook.com>
2017-02-22 00:58:19 -0500

    mongodb connector class

---
+++ src/project1/mongodb_connector.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: <encoding name> -*-
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# Copyright 2017 Jason Qiao Meng
+
+__doc__ = """mongodb_connector.py provides a conventient way to get a MongoDB
+database object.
+"""
+
+from settings import DB_NAME, DB_PROTOCOL, DB_HOST, DB_PORT, COLLECTION
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure, InvalidName
+
+
+class MongoDB(object):
+    """A class to connect to a MongoDB according to the settings.py"""
+
+    def __init__(self):
+        """Create and initialize an instance of class MongoDB"""
+        self.__conn_str = None
+        self.__client = None
+        self.__db = None
+
+    def simple_connction_string(self):
+        """Get the connection string.
+
+        :return: A legal MongoDB connection string from the settings.
+        """
+        self.__conn_str = "%s%s:%s/" % (DB_PROTOCOL, DB_HOST, DB_PORT)
+        return __conn_str
+
+    def get_client(self):
+        """Connect to the MongoDB and return a client object.
+        Refer to: http://api.mongodb.com/python/current/api/pymongo/mongo_client.html#pymongo.mongo_client.MongoClient
+
+        :return: An instance of class MongoClient
+        """
+        if self.__client:
+            return __client
+
+        self.__client = MongoClient(self.simple_connction_string())
+        try:
+            # The ismaster command is cheap and does not require auth.
+            client.admin.command('ismaster')
+        except ConnectionFailure as cf:
+            print 'Connecting to MongoDB failed: ', cf
+            self.__client = None
+
+        return self.__client
+
+    def get_database(self, name=None):
+        """Get the database object with the specified name.
+
+        :param name: The name of the database. If given None or omitted,
+                     this method use the name set in the settings file.
+        :return: An instance of Database.
+        """
+        if not self.__client:
+            self.get_client()
+
+        dbname = name if name else DB_NAME
+        try:
+            self.__db = self.__client[dbname]
+        except InvalidName as ine:
+            self.__db = None
+            print 'No such database: %s. %s' % (dbname, ine)
+
+        return self.__db
---
+++ src/project1/settings.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: <encoding name> -*-
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# Copyright 2017 Jason Qiao Meng
+
+__doc__ = """This file contains the global settings for this program.
+"""
+
+# database related
+DB_NAME = "zipcodes"
+DB_PROTOCOL = "mongodb://"
+DB_HOST = "localhost"
+DB_PORT = "27017"
+COLLECTION = "zipcodes"
+
+
+# unit tests
+from pymongo import MongoClient
+
+if __name__ == '__main__':
+    conn_string = "%s%s:%s/" % (DB_PROTOCOL, DB_HOST, DB_PORT);
+    print conn_string
+
+    # test mongoDB connection
+    client = MongoClient(conn_string)
+    for db in client.database_names():
+        print '%s, ' % db ,
